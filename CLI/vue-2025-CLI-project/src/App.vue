615cef0b379c2362ce058f9267f75617cbddb8d6
Sabrina_Rashid <saashid18@gmail.com>
2025-05-21 18:10:39 +0600

    v-if, v-else-if, v-if derectives

--- CLI/vue-2025-CLI-project/src/App.vue
+++ CLI/vue-2025-CLI-project/src/App.vue
@@ -3,12 +3,16 @@ export default{
   data(){
     return {
       name: "sabrina sabrina",
+      status: 'active',
     };
   },
 };
 </script>
 <template>
 <h1>{{name}}</h1>
+<p v-if="status === 'active'">User is active</p>
+<p v-else-if="status === 'pending'">User is pending</p>
+<p v-else>User is inactive</p>
 </template>
 
 
