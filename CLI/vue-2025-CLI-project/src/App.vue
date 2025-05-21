f32ba468579e1fda21b2d2fb84ea089f140ca558
Sabrina_Rashid <saashid18@gmail.com>
2025-05-21 18:21:13 +0600

    v-for derectives

--- CLI/vue-2025-CLI-project/src/App.vue
+++ CLI/vue-2025-CLI-project/src/App.vue
@@ -2,17 +2,27 @@
 export default{
   data(){
     return {
-      name: "sabrina sabrina",
+      name: "sabrina sabrina",// optional api data() and interpolation
       status: 'active',
+      tasks: ['task one','task two','task three','task four','task five']
     };
   },
 };
 </script>
 <template>
-<h1>{{name}}</h1>
+ <h1>{{name}}</h1><!-- interpolation -->
+
+ <!-- directives  -->
+  <!-- contional v-if, v-else-if, v-else  -->
 <p v-if="status === 'active'">User is active</p>
 <p v-else-if="status === 'pending'">User is pending</p>
 <p v-else>User is inactive</p>
+
+ <!-- loop v-for  -->
+  <h1>Tasks:</h1>
+  <ul>
+    <li v-for="task in tasks" key="task">{{ task }} </li>
+  </ul>
 </template>
 
 
